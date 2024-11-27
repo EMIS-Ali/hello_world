738e8b6f5760d3b90fcb8a239174dc108fa45251
EMIS_ALI <m.ali@emi-solutions.biz>
2024-11-27 16:00:16 +0800

    getX counter app

--- main.dart
+++ main.dart
@@ -1,69 +1,135 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// Counter Controller
+class CounterController extends GetxController {
+  // Observable variable
+  var count = 0.obs;
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  // Method to increment counter
+  void increment() {
+    count++;
+  }
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  // Method to decrement counter
+  void decrement() {
+    if (count > 0) {
+      count--;
+    }
+  }
+
+  // Method to reset counter
+  void reset() {
+    count.value = 0;
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+// Details Screen
+class DetailsScreen extends StatelessWidget {
+  // Get the same instance of CounterController
+  final CounterController controller = Get.find<CounterController>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text('Counter Details'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+          children: [
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Counter Details Page',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            // Using Obx to monitor changes
+            Obx(() => Text(
+                  'Current Count: ${controller.count}',
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                )),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => Get.back(),
+              child: Text('Go Back'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+}
+
+// Home Screen
+class HomeScreen extends StatelessWidget {
+  // Initialize controller using Get.put()
+  final CounterController controller = Get.put(CounterController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GetX Counter App'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () => controller.reset(),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'You have pushed the button this many times:',
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20),
+            // Using Obx to monitor changes
+            Obx(() => Text(
+                  '${controller.count}',
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                )),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                  onPressed: controller.decrement,
+                  tooltip: 'Decrement',
+                  child: Icon(Icons.remove),
+                ),
+                FloatingActionButton(
+                  onPressed: controller.increment,
+                  tooltip: 'Increment',
+                  child: Icon(Icons.add),
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () => Get.to(() => DetailsScreen()),
+              child: Text('Go to Details'),
+            ),
+          ],
+        ),
       ),
     );
   }
+}
+
+// Main App
+void main() {
+  runApp(
+    GetMaterialApp(
+      title: 'GetX Counter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: HomeScreen(),
+    ),
+  );
 }
